8a88595183a6fac36fa86b2dfe5492a3ac59d0dc
humoflife <markus.schweig@upbound.io>
2023-10-22 17:17:04 -0700

    moved make provider script

--- Makefile
+++ Makefile
@@ -74,7 +74,7 @@ bootstrap: e2e
 	# kubectl apply -f test/provider/aws.yaml
 	# kubectl wait provider.pkg --all --for condition=Healthy --timeout=15m 2>/dev/null
 
-	# config/prometheus/scripts/make-provider-service.sh
+	# scripts/prometheus-make-provider-service.sh
 	# kubectl apply -f examples/oss.yaml
 
 	# OPERATORS_NAMESPACE=""
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ make e2e
 kubectl apply -f test/provider/aws.yaml
 kubectl wait provider.pkg --all --for condition=Healthy --timeout=15m 2>/dev/null
 
-config/prometheus/scripts/make-provider-service.sh
+scripts/prometheus-make-provider-service.sh
 kubectl apply -f examples/oss.yaml
 
 OPERATORS_NAMESPACE=""
@@ -14,5 +14,12 @@ while [[ "${OPERATORS_NAMESPACE}" == "" ]]; do
 done
 
 kubectl apply -f config/operators-config
-sleep 5
-kubectl wait -n operators --for=condition=Ready pod -l app.kubernetes.io/name=prometheus
+
+PROMETHEUS_READY=""
+while [[ "${PROMETHEUS_READY}" == "" ]]; do
+    sleep 3
+    PROMETHEUS_READY=$(kubectl wait -n operators \
+	--for=condition=Ready pod \
+	-l app.kubernetes.io/name=prometheus \
+	2>/dev/null)
+done
