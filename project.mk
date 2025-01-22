cd249fed6af32c8356147f382110ecdab9358f30
Yury Tsarev <yury@upbound.io>
2025-01-22 20:24:16 +0100

    Add project.mk
    Signed-off-by: Yury Tsarev <yury@upbound.io>

---
+++ project.mk
@@ -0,0 +1,4 @@
+PROJECT_NAME := configuration-observability-oss
+UPTEST_INPUT_MANIFESTS := examples/folder-grafana.yaml,examples/dashboard-grafana-crossplane-health.yaml,examples/dashboard-grafana-crossplane-mr.yaml,examples/dashboard-grafana-crossplane-resources-ttr.yaml,examples/dashboard-grafana-crossplane-sli-metrics.yaml,examples/dashboard-grafana-crossplane-sli-2-metrics.yaml,examples/dashboard-grafana-crossplane-top-level.yaml,examples/dashboard-grafana-crossplane-xmetrics.yaml,examples/oss.yaml
+UPTEST_SKIP_IMPORT := true
+UPTEST_SKIP_UPDATE := true
